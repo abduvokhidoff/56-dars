92efc9bfb354f15e75749902c7ead13389f5a43a
abduvokhidoff <abcdefgmananam@gmail.com>
2024-09-29 22:36:38 +0500

    done

---
+++ main.js
@@ -0,0 +1,9 @@
+let num = prompt('Ismingzni kiriting!')
+let letter = prompt('Harifingizni Kiriting!')
+
+if(num.includes(letter)){
+alert(`${num}-bu sozda ${letter} harifi bor`)
+}
+else{
+    alert(`${num}-bu sozda ${letter} harifi yoq`)
+}
